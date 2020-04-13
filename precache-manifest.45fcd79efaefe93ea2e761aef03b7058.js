e768f84e3a553e18daa022f8262fdcad0d3bc4d2
HatemRamadan <hatemram96@gmail.com>
2020-04-13 23:10:16 +0200

    Updates

---
+++ precache-manifest.45fcd79efaefe93ea2e761aef03b7058.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43223461800400053a843cb965be0671",
+    "url": "/covid19-detector/index.html"
+  },
+  {
+    "revision": "cf54aa16f3642a2f36e5",
+    "url": "/covid19-detector/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7d4be86dd6f0db8a4921",
+    "url": "/covid19-detector/static/js/2.7f2ccb85.chunk.js"
+  },
+  {
+    "revision": "26f3782ee2edf5e6e73adb27872b7a31",
+    "url": "/covid19-detector/static/js/2.7f2ccb85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf54aa16f3642a2f36e5",
+    "url": "/covid19-detector/static/js/main.a544a40a.chunk.js"
+  },
+  {
+    "revision": "d567c1f120ae485bf099",
+    "url": "/covid19-detector/static/js/runtime-main.a5ff1301.js"
+  }
+]);
